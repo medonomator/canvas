69530b9cc482956e36bb4a644fe1c0070d1c0ff5
medonomator <budteschastlivi1@gmail.com>
2017-04-13 17:47:22 +0300

    partical

--- src/main.js
+++ src/main.js
@@ -11,29 +11,11 @@ let h = canvas.height;
 // button
 knopki()
 
-let user = new USER(30, 30, 30, 30);
 
 function gameLoop() {
   ctx.clearRect(0, 0,w ,h);
 
-  move()
+
   requestAnimationFrame(gameLoop)
 }
 gameLoop()
-
-function move() {
-  if(Right) user.x += 5;
-  if(Left)  user.x -= 5;
-  if(Up)    user.y -= 5;
-  if(Down)  user.y += 5;
-}
-
-// let sprite = new Image();
-// sprite.src = require('./img/tom.png');
-//
-//
-// let sound = new Audio();
-// sound.src = require('./sound/Laser_Shoot.wav');
-
-
-// matrix
