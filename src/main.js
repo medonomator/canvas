8a883e8b7c959fa0115f0859bb8fdf74c3e092c7
medonomator <budteschastlivi1@gmail.com>
2017-04-13 17:42:09 +0300

    snake

--- src/main.js
+++ src/main.js
@@ -1,19 +1,35 @@
 import { knopki, Right, Left, Up, Down }  from './initialSctipt/knopki';
+import { USER, CUB } from './InitialObject/InitialObject';
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 let w = canvas.width;
 let h = canvas.height;
-
-
+let snake = [1,2,3];
+let matrix = [];
 // my snake game
+for (var i = 0; i < 10; i++) {
+  matrix.push([])
+  for (var j = 0; j < 10; j++) {
+    matrix[i].push(j)
+  }
+}
 
 // button
 knopki()
 
+let user = new USER(30, 30, 30, 30);
+
 function gameLoop() {
   ctx.clearRect(0, 0,w ,h)
 
+
+  matrix.map((item, index) => {
+  
+    item.map((item2, index2) => {
+      ctx.fillRect(index*32,index2*32,30,30);
+    })
+  })
 
   move()
   requestAnimationFrame(gameLoop)
@@ -27,11 +43,12 @@ function move() {
   if(Down)  user.y += 5;
 }
 
-
-
 // let sprite = new Image();
 // sprite.src = require('./img/tom.png');
 //
 //
 // let sound = new Audio();
 // sound.src = require('./sound/Laser_Shoot.wav');
+
+
+// matrix
